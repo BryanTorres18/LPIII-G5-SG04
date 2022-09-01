559039fa586c63c277f86f3bec330034805f7872
BryanTorres18 <bryan.torres@ucsm.edu.pe>
2022-09-01 18:09:25 -0500

    Cambios en las carpetas

---
+++ Sesion 3/ACTIVIDADES/2 y 3/Contador/src/contador/Contador.java
@@ -0,0 +1,29 @@
+package contador;
+
+public class Contador {
+    static int acumulador = 0;
+    private int valor;
+    
+    public static int acumulador()
+    {
+        return acumulador;
+    }
+    
+    public Contador(int valor)
+    {
+        this.valor = valor;
+        Contador.acumulador += valor;
+    }
+    
+    public void inc()
+    {
+        valor++;
+        acumulador++;
+    }
+    
+    public int getValor()
+    {
+        return this.valor;
+    }
+    
+}
