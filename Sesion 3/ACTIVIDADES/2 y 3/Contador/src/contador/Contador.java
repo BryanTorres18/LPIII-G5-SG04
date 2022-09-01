c2fe4288d23d9987ed0b312532de2e396e8246d2
BryanTorres18 <bryan.torres@ucsm.edu.pe>
2022-09-01 18:34:10 -0500

    realizando cambios en actividades parte 3

--- Sesion 3/ACTIVIDADES/2 y 3/Contador/src/contador/Contador.java
+++ Sesion 3/ACTIVIDADES/2 y 3/Contador/src/contador/Contador.java
@@ -1,7 +1,7 @@
 package contador;
 
 public class Contador {
-    static int acumulador = 0;
+    private static int acumulador = 0;
     private int valor;
     
     public static int acumulador()
