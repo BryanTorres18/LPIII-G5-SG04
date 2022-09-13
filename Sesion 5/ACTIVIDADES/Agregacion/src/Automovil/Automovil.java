62b5bc252423e544e5474aabd8e9a1288ab287a7
-- <bryan.torres@ucsm.edu.pe>
2022-09-13 06:22:39 -0500

    Actividad 2 - parte 1

---
+++ Sesion 5/ACTIVIDADES/Agregacion/src/Automovil/Automovil.java
@@ -0,0 +1,18 @@
+
+package Automovil;
+
+public class Automovil {
+
+    private String placa;
+    private int numPuertas;
+    private String marca;
+    private String modelo;
+    private Motor motor;
+
+    public Automovil(String placa, int nPuertas, String marca, String modelo) {
+        this.placa = placa;
+        this.numPuertas = nPuertas;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+}
---
+++ Sesion 5/ACTIVIDADES/Agregacion/src/Automovil/Motor.java
@@ -0,0 +1,6 @@
+
+package Automovil;
+
+public class Motor {
+    
+}
