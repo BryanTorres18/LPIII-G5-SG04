a1c40e0309459dd20779c07595d6a7a720f39e85
-- <bryan.torres@ucsm.edu.pe>
2022-09-13 06:24:35 -0500

    Actividad 2 - Parte 2

--- Sesion 5/ACTIVIDADES/Agregacion/src/Automovil/Automovil.java
+++ Sesion 5/ACTIVIDADES/Agregacion/src/Automovil/Automovil.java
@@ -15,4 +15,58 @@ public class Automovil {
         this.marca = marca;
         this.modelo = modelo;
     }
+    
+    // Getter y Setter
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public int getNumPuertas() {
+        return numPuertas;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public void setNumPuertas(int numPuertas) {
+        this.numPuertas = numPuertas;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setMotor(Motor motor) {
+        this.motor = motor;
+    }
+    
+    //ToString
+    @Override
+    public String toString() {
+        return "Automovil{" + "placa=" + placa + ", numPuertas=" + numPuertas + ", marca=" + marca + ", modelo=" + modelo + ", motor=" + motor + '}';
+    }
+    
+    
+    
 }
+
+
+
