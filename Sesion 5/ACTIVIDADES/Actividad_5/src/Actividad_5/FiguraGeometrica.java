05273b02b13f728eb4004c31c386d0ac60698750
carlosavila <carlos.avila@ucsm.edu.pe>
2022-09-15 17:53:10 -0500

    actividad 5 terminado

--- Sesion 5/ACTIVIDADES/Actividad_5/src/Actividad_5/FiguraGeometrica.java
+++ Sesion 5/ACTIVIDADES/Actividad_5/src/Actividad_5/FiguraGeometrica.java
@@ -1,5 +1,21 @@
 package Actividad_5;
 
-public class FiguraGeometrica {
-    
+public abstract class FiguraGeometrica {
+
+    private String nombre;
+
+    abstract public double area();
+
+    public FiguraGeometrica(String nombreFigura) {
+        this.nombre = nombreFigura;
+    }
+
+    final public boolean mayorQue(FiguraGeometrica otra) {
+        return this.area() > otra.area();
+    }
+
+    @Override
+    final public String toString(){
+        return this.nombre + " con area " + this.area();
+    }
 }
