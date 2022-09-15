86c952249f1f427f7cbc0a74aa864685bec7f3e6
carlosavila <carlos.avila@ucsm.edu.pe>
2022-09-15 18:54:44 -0500

    terminado

--- Sesion 5/ACTIVIDADES/Actividad_3/src/Actividad_3/TestAsociacion.java
+++ Sesion 5/ACTIVIDADES/Actividad_3/src/Actividad_3/TestAsociacion.java
@@ -1,9 +1,15 @@
 package Actividad_3;
-
 public class TestAsociacion {
     public static void main(String[] args){
         Banco bcp = new Banco("BCP");
         Banco bbva =  new Banco("BBVA", 30);
+        Persona p1 = new Persona(16513, "Carlos", "Avila");
+        Persona p2 = new Persona(98415, "Bryan", "Torres");
+        Persona p3 = new Persona(98745, "Manuel", "Garcia");
         
+        bcp.agregarCliente(p1);
+        bbva.agregarCliente(p2);
+        bcp.darBajaCliente(p1);
+        System.out.println(bcp.buscarCliente(p3));
     }
 }
