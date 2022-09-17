d2e2a7f29bde79deb7168d41f966f6b8357d2a9b
BryanTorres18 <90003001+BryanTorres18@users.noreply.github.com>
2022-09-17 13:36:09 -0500

    Terminado el Ejercicio_1

--- Sesion 5/EJERCICIOS/Ejercicio_1/src/Cuenta.java
+++ Sesion 5/EJERCICIOS/Ejercicio_1/src/Cuenta.java
@@ -1,16 +1,14 @@
-package Actividad_1;
-
 public class Cuenta{
     private int numero;
     private double saldo;
-    private char tipoCliente = 'C';
+    private char tipoCliente;
     
-    public Cuenta(int numero, double saldo){
+    public Cuenta(int numero, double saldo, char tipoCliente){
         this.numero=numero;
         this.saldo=saldo;
     }
-    public Cuenta(int numero){
-        this(numero, 0);
+    public Cuenta(int numero,char cliente){
+        this(numero,50, cliente);
     }
     
     //GETS
@@ -20,7 +18,11 @@ public class Cuenta{
     public double getSaldo(){
         return saldo;
     }
-    
+
+    public char getTipoCliente() {
+        return tipoCliente;
+    }
+
     //SETS
     public void setNumero(int numero){
         this.numero=numero;
@@ -28,7 +30,12 @@ public class Cuenta{
     public void setSaldo(double saldo){
         this.saldo=saldo;
     }
-    
+
+    public void setTipoCliente(char tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+
     @Override
     public String toString(){
         return "Cuenta{" + "numero= "+numero+" saldo= "+saldo+"}";
--- Sesion 5/EJERCICIOS/Ejercicio_1/src/TestComposicion.java
+++ Sesion 5/EJERCICIOS/Ejercicio_1/src/TestComposicion.java
@@ -1,6 +1,6 @@
-package Actividad_1;
 public class TestComposicion {
     public static void main(String[] args){
+        /*
         Persona p1 = new Persona(1234,"Carlos","Avila");
         Persona p2 = new Persona(7845,"Bryan","Torres");
         System.out.println("CARLOS: Cuenta y Saldo actual: ");
@@ -16,5 +16,19 @@ public class TestComposicion {
         System.out.println("BRYAN: Cuenta y Saldo actualizada: ");
         p2.getCuenta().setSaldo(250.00);
         System.out.println(p2.getCuenta().getSaldo());
+         */
+
+        Persona p1 = new Persona(1234, "Juan", "Sanchez", 'C');
+        Persona p2 = new Persona(4213, "Maria", "Rosarios", 'H');
+        Persona p3 = new Persona(5521, "Daniel", "Paredes", 'B');
+        System.out.println(p1);
+        System.out.println("===================");
+        System.out.println(p2);
+        System.out.println("===================");
+        System.out.println(p3);
+        System.out.println("===================");
+        p1.depositar(50000);
+        p1.retirar(1000);
+
     }
 }
