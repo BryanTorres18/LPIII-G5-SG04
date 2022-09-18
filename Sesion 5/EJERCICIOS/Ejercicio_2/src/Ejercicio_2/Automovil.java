26d2942b239c595a5d3f3ea7e2f4ec47d37f117d
carlosavila <carlos.avila@ucsm.edu.pe>
2022-09-17 21:42:16 -0500

    creacion de archivos

---
+++ Sesion 5/EJERCICIOS/Ejercicio_2/src/Ejercicio_2/Automovil.java
@@ -0,0 +1,71 @@
+package Ejercicio_2;
+
+public class Automovil {
+
+    private String placa;
+    private int numPuertas;
+    private String marca;
+    private String modelo;
+    private Motor motor;
+
+    public Automovil(String placa, int nPuertas, String marca, String modelo) {
+        this.placa = placa;
+        this.numPuertas = nPuertas;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+    
+    // Getter y Setter
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public int getNumPuertas() {
+        return numPuertas;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public void setNumPuertas(int numPuertas) {
+        this.numPuertas = numPuertas;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setMotor(Motor motor) {
+        this.motor = motor;
+    }
+    
+    //ToString
+    @Override
+    public String toString() {
+        return "Automovil{" + "placa=" + placa + ", numPuertas=" + numPuertas + ", marca=" + marca + ", modelo=" + modelo + ", motor=" + motor + '}';
+    }
+    
+    
+    
+}
+
+
+
---
+++ Sesion 5/EJERCICIOS/Ejercicio_2/src/Ejercicio_2/Motor.java
@@ -0,0 +1,39 @@
+package Ejercicio_2;
+
+public class Motor {
+    private int numMotor;
+    private int revPorMin;
+    
+    public Motor(int numMotor, int revPorMin)
+    {
+        this.numMotor = numMotor;
+        this.revPorMin = revPorMin;
+    }
+    
+    //Getter y Setter
+
+    public int getNumMotor() {
+        return numMotor;
+    }
+
+    public int getRevPorMin() {
+        return revPorMin;
+    }
+
+    public void setNumMotor(int numMotor) {
+        this.numMotor = numMotor;
+    }
+
+    public void setRevPorMin(int revPorMin) {
+        this.revPorMin = revPorMin;
+    }
+
+    //ToString
+    @Override
+    public String toString() {
+        return "Motor{" + "numMotor=" + numMotor + ", revPorMin=" + revPorMin + '}';
+    }
+    
+    
+    
+}
