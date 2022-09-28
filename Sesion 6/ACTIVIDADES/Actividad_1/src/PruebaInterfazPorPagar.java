9b2dbaec0bdd7c34f358aa0e70e14a740baacd39
BryanTorres18 <90003001+BryanTorres18@users.noreply.github.com>
2022-09-28 18:44:23 -0500

    Actividad - Parte 1

---
+++ Sesion 6/ACTIVIDADES/Actividad_1/src/PruebaInterfazPorPagar.java
@@ -0,0 +1,18 @@
+public class PruebaInterfazPorPagar {
+    public static void main(String[] args) {
+        PorPagar[] objPorPagar = new PorPagar[4];
+
+        objPorPagar[0] = new Factura("01234", "asiento", 2, 375.00);
+        objPorPagar[1] = new Factura("56789", "llanta", 4, 79.95);
+        objPorPagar[2] = new EmpleadoAsalariado("John", "Smith", "111-11-111",
+                800.00);
+        objPorPagar[3] = new EmpleadoAsalariado("Lisa", "Barnes", "888-88-1111", 1200.00);
+
+        System.out.println("Facturas y Empleados procesados en forma polimorfica: \n");
+
+        for(PorPagar porPagarActual : objPorPagar)
+        {
+            System.out.printf("%s \n%s: $%,.2f\n\n", porPagarActual.toString(), "Pago vencido", porPagarActual.obtenerMontoPago());
+        }
+    }
+}
