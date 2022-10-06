2ebd0e5ea624bd962f10cadfaabac7cc41bc51ff
carlosavila <carlos.avila@ucsm.edu.pe>
2022-10-06 18:38:20 -0500

    primer avance

---
+++ Sesion 7/ACTIVIDADES/Actividad_1/src/Actividad_1/App.java
@@ -0,0 +1,21 @@
+package Actividad_1;
+
+import java.util.*;
+
+public class App {
+    public static int cociente(int numerador, int denominador){
+        return numerador/denominador;
+    }
+    public static void main(String[] args){
+        App a;
+       Scanner explorador = new Scanner(System .in);
+       System.out.println("Introduzca un numerador entero: ");
+       int numerador = explorador.nextInt();
+       System.out.println("Introduzca un denominador entero: ");
+       int denominador = explorador.nextInt();
+       
+       int resultado = cociente(numerador, denominador);
+       System.out.println("\nResultado: %d/%d = %d\n", numerador, denominador, resultado);
+   } 
+}
+
