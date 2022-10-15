3ba7e8e4ce5a3f3b4cf20cbec3fd489079d7336e
carlosavila <carlos.avila@ucsm.edu.pe>
2022-10-14 20:03:58 -0500

    1ra parte

--- Sesion 8/TAREA/practica3/src/practica/MarcoRastreadorRaton.java
+++ Sesion 8/TAREA/practica3/src/practica/MarcoRastreadorRaton.java
@@ -4,46 +4,52 @@ import java.awt.Color;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.*;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
+import javax.swing.*;
 
-public class MarcoRastreadorRaton extends JFrame {
+public class MarcoRastreadorRaton extends JFrame implements MouseListener, MouseMotionListener{
 
     private final JLabel etiqueta;
     private final JTextField campoTexto;
-    private final JLabel lb;
-    private final JLabel lb1;
+    private JLabel lb;
+    private JLabel lb1;
     public MarcoRastreadorRaton() {
         super("Demostracion de los eventos de raton");
-        setLayout(new FlowLayout());
-        Icon imag = new ImageIcon("barnie.png");
-        Icon imag1 = new ImageIcon("barnie1.png");
-        etiqueta = new JLabel("Etiqueta con texto e icono", imag, SwingConstants.LEFT);
-        campoTexto = new JTextField(10);
-        lb = new JLabel(imag);
-        lb1 = new JLabel(imag1);
+        this.setLayout(new FlowLayout());
+               
+        Icon imag = new ImageIcon(getClass().getResource("click1.png"));
+        Icon imag1 = new ImageIcon(getClass().getResource("click2.png"));
         
-        add(lb);
-        add(lb1);
-        add(campoTexto);
+        etiqueta = new JLabel("Ingrese su nombre", imag,SwingConstants.LEFT);
+        etiqueta.setToolTipText("etiqueta");
         add(etiqueta);
+        
+        campoTexto = new JTextField(10);
+        add(campoTexto);
+        
+        //lb.setIcon(imag);
+        //this.add(lb);
+        //this.pack();
+        //this.setLocationRelativeTo(null);
+        //this.setVisible(true);
+        
+        //lb1.setIcon(imag1);
+        //this.add(lb1);
+        //this.pack();
+        //this.setLocationRelativeTo(null);
+        //this.setVisible(true);
+        
+        
+        
+        
+
         campoTexto.setBackground(Color.gray);
         add(campoTexto, BorderLayout.PAGE_END);
-        
-        ManejadorRaton manejador = new ManejadorRaton();
-        campoTexto.addMouseListener(manejador);
-        campoTexto.addMouseMotionListener(manejador);
-        lb.addMouseListener(manejador);
-        lb1.addMouseListener(manejador);
+        //lb.addMouseListener(this);
+        //lb1.addMouseListener(this);
+        campoTexto.addMouseListener(this);
 
     }
 
-    private class ManejadorRaton implements MouseListener, MouseMotionListener {
 // Los manejadores de eventos de MouseListener
 // manejan el evento cuando se suelta el ratón justo después de oprimir el botón
 
@@ -54,8 +60,7 @@ public class MarcoRastreadorRaton extends JFrame {
 // maneja evento cuando se oprime el ratón
         @Override
         public void mousePressed(MouseEvent evento) {
-            Icon imag1 = new ImageIcon("barnie1.png");
-            lb1.setIcon(imag1);
+            
         }
 
 // maneja evento cuando se suelta el botón del ratón
@@ -66,15 +71,16 @@ public class MarcoRastreadorRaton extends JFrame {
 // maneja evento cuando el ratón entra al área
         @Override
         public void mouseEntered(MouseEvent evento) {
-            campoTexto.setBackground(Color.GRAY);
+            campoTexto.setBackground(Color.WHITE);
+            
+            
         }
 
 // maneja evento cuando el ratón sale del área
         @Override
         public void mouseExited(MouseEvent evento) {
-            campoTexto.setBackground(Color.WHITE);
-            Icon imag = new ImageIcon("barnie.png");
-            lb.setIcon(imag);
+            campoTexto.setBackground(Color.GRAY);
+            //lb.setIcon(imag);
         }
 
 // Los manejadores de eventos de MouseMotionListener manejan
@@ -87,5 +93,5 @@ public class MarcoRastreadorRaton extends JFrame {
         @Override
         public void mouseMoved(MouseEvent evento) {
         }
-    } // fin de la clase interna ManejadorRaton
 } // fin de la clase MarcoRastreadorRaton
+
--- Sesion 8/TAREA/practica3/src/practica/main.java
+++ Sesion 8/TAREA/practica3/src/practica/main.java
@@ -11,3 +11,4 @@ public class main {
         marcoRastreadorRaton.setVisible(true);
     }
 }
+
