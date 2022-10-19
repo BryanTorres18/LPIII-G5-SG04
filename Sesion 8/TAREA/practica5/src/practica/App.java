a93775b9cfdd0c353bbf6ae5f420a36369903032
carlosavila <carlos.avila@ucsm.edu.pe>
2022-10-18 20:59:25 -0500

    falta poner los colores al JTextArea

---
+++ Sesion 8/TAREA/practica5/src/practica/App.java
@@ -0,0 +1,14 @@
+package practica;
+
+import javax.swing.JFrame;
+
+public class App {
+
+    public static void main(String[] args) {
+        MarcoDemoTeclas marcoDemoTeclas = new MarcoDemoTeclas();
+        marcoDemoTeclas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marcoDemoTeclas.setSize(350, 100);
+        marcoDemoTeclas.setVisible(true);
+    }
+} // fin de la clase DemoTeclas
+
