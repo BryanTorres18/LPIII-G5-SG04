b5a6f1a2395066d78f7c62787047828a9fa39aca
carlosavila <carlos.avila@ucsm.edu.pe>
2022-10-30 00:54:13 -0500

    Ultimas modificaciones

--- Sesion 9/EJERCICIOS/Ejercicio2/src/Ejercicio2/main.java
+++ Sesion 9/EJERCICIOS/Ejercicio2/src/Ejercicio2/main.java
@@ -2,11 +2,14 @@ package Ejercicio2;
 
 import java.awt.Color;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class main extends JFrame {
 
@@ -16,34 +19,46 @@ public class main extends JFrame {
     JLabel ePalabras = new JLabel();
     JLabel eCaracteres = new JLabel();
     JLabel titulo = new JLabel();
+    JTextArea areaTexto = new JTextArea();
 
-    public main(WordCount wc) {
-        
+    public main(WordCount wc) throws FileNotFoundException, IOException {
+
         jpanel.setLayout(null);
-        jpanel.setBackground(Color.lightGray);
+        jpanel.setBackground(Color.white);
+
+        titulo.setText("CONTENIDO DEL ARCHIVO");
+        titulo.setBounds(170, 15, 220, 21);
         
-        titulo.setText("LINEAS - PALABRAS - CARACTERES");
-        titulo.setBounds(100, 15, 220, 21);
+        byte by[] = new byte[1024];
+        FileInputStream file = new FileInputStream(wc.getFichero());
+        file.read(by);
+        String s = new String(by);
         
+        areaTexto = new JTextArea(s, 50, 50);
+        JScrollPane scroll = new JScrollPane(areaTexto);
+        scroll.setBounds(80, 50, 320, 200);
+
         eArchivo.setText("Nombre del Archivo: " + wc.getFichero().getName());
-        eArchivo.setBounds(5, 50, 220, 21);      
-        
+        eArchivo.setBounds(5, 270, 220, 21);
+
         eLineas = new JLabel("Total de lineas: " + wc.totalLineas());
-        eLineas.setBounds(5, 70, 220, 21); 
-        
+        eLineas.setBounds(5, 290, 220, 21);
+
         ePalabras = new JLabel("Total de palabras: " + wc.totalPalabras());
-        ePalabras.setBounds(5, 90, 220, 21); 
-        
+        ePalabras.setBounds(5, 310, 220, 21);
+
         eCaracteres = new JLabel("Total de caracteres: " + wc.totalCaracteres());
-        eCaracteres.setBounds(5, 110, 220, 21); 
-        
+        eCaracteres.setBounds(5, 330, 220, 21);
+
         jpanel.add(titulo, null);
+        jpanel.add(scroll, null);
         jpanel.add(eArchivo, null);
         jpanel.add(eLineas, null);
         jpanel.add(ePalabras, null);
         jpanel.add(eCaracteres, null);
         
-        setSize(500, 450);
+
+        setSize(500, 500);
         setTitle("Contador de Lineas, Palabras y Letras");
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
