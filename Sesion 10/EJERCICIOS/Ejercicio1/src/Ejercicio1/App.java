5fe1a2be73fb512517528679af730fffe1a5922d
carlosavila <carlos.avila@ucsm.edu.pe>
2022-11-14 19:46:05 -0500

    Ejercicio 1 terminado

---
+++ Sesion 10/EJERCICIOS/Ejercicio1/src/Ejercicio1/App.java
@@ -0,0 +1,38 @@
+package Ejercicio1;
+
+/**
+ *
+ * @author USUARIO
+ *
+ */
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        String[] Cadena={"Uno\t","Dos\t","Tres\t","Cuatro\t","Cinco\t","Seis\t","Siete\t","Ocho\t"};
+        System.out.printf("Array de cadenas contiene:\n");
+        imprimirArray(Cadena);
+
+    }
+
+    public static <T> void imprimirArray(T[] inputArray) {
+        for (T elemento : inputArray) {
+            System.out.printf("%s ", elemento);
+        }
+    }
+    
+    public static String imprimirArray(String[] cadena){
+        int cont=0;
+        for (String elemento : cadena) {
+            cont++;
+            System.out.printf("%s ", elemento);
+            if (cont == 4)
+                System.out.printf("\n");
+        }
+        System.out.println("\n");
+    return "";
+    }
+
+}
+ 
